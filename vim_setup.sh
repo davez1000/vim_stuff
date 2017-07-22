9e4ba9daf8764c0f55f3d1fceb84a57303fa4d24
David Brown <davez1000@gmail.com>
2017-07-22 18:42:57 +1000

    Added vim-airline to installs.

--- vim_setup.sh
+++ vim_setup.sh
@@ -12,7 +12,7 @@ brew install fzf
 mkdir -p $BUNDLEDIR
 mkdir -p $AUTOLOADDIR
 
-PACKAGES=('tpope/vim-fugitive' 'junegunn/fzf.vim' 'junegunn/fzf' 'scrooloose/nerdtree' 'easymotion/vim-easymotion')
+PACKAGES=('tpope/vim-fugitive' 'junegunn/fzf.vim' 'junegunn/fzf' 'scrooloose/nerdtree' 'easymotion/vim-easymotion' 'vim-airline/vim-airline')
 
 git clone https://github.com/tpope/vim-pathogen && mv $BASE/vim-pathogen/autoload/pathogen.vim $AUTOLOADDIR && rm -rf $BASE/vim-pathogen
 
