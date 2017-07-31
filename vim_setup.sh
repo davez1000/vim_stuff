c17c021fb3d97bac113395fd42bfe731d67834ac
David Brown <david.brown@xvt.com.au>
2017-07-31 11:09:44 +1000

    vimrc.

--- vim_setup.sh
+++ vim_setup.sh
@@ -4,6 +4,7 @@ BASE=~
 BUNDLEDIR=$BASE/.vim/bundle
 AUTOLOADDIR=$BASE/.vim/autoload
 BACKUPDIR=$BASE/.vim/backups
+VIMRCDIR=$BASE/vimrc
 
 # Install some binaries
 brew update
@@ -13,8 +14,18 @@ brew install fzf
 mkdir -p $BUNDLEDIR
 mkdir -p $AUTOLOADDIR
 mkdir -p $BACKUPDIR
+mkdir -p $VIMRCDIR
 
-PACKAGES=('tpope/vim-fugitive' 'junegunn/fzf.vim' 'junegunn/fzf' 'scrooloose/nerdtree' 'easymotion/vim-easymotion' 'vim-airline/vim-airline' 'MattesGroeger/vim-bookmarks')
+PACKAGES=( \
+'tpope/vim-fugitive' \
+'junegunn/fzf.vim' \
+'junegunn/fzf' \
+'scrooloose/nerdtree' \
+'easymotion/vim-easymotion' \
+'vim-airline/vim-airline' \
+'vim-syntastic/syntastic' \
+'airblade/vim-gitgutter' \
+)
 
 git clone https://github.com/tpope/vim-pathogen && mv $BASE/vim-pathogen/autoload/pathogen.vim $AUTOLOADDIR && rm -rf $BASE/vim-pathogen
 
@@ -23,3 +34,4 @@ do
   git clone https://github.com/$i.git $BUNDLEDIR/`echo $i | cut -d'/' -f2`
 done
 
+#ln -s $VIMRCBASE/.vimrc .vimrc
